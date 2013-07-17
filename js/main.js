23a7b0bd7317951f00819443e3b3ac02b3ee5ddc
binwilly-w <Gaucho@Gauchos-Mac-mini.local>
2013-07-17 18:37:57 -0300

    sammy js added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
 
+(function($) {
+  var app = $.sammy(function() {
+  	this.use('Template');
+
+    this.get('#/', function() {
+      $('#content').text('Home');
+      //this.('templates/home.template');
+    });
+
+    this.get('#/gallery', function() {
+      $('#content').text('Hello World');
+    });
+
+	this.get('#/about', function() {
+      $('#content').text('About');
+    });
+
+  });
+
+  $(function() {
+    app.run('#/')
+  });
+})(jQuery);
