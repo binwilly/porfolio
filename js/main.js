70cabbc4b549bb1334e6a9dfdf930d124aa232de
Binwilly <binwilly@gmail.com>
2013-07-31 10:06:37 -0300

    trying to add animation

--- js/main.js
+++ js/main.js
@@ -17,38 +17,42 @@ PROFILE_WEB.contentToHide = function(content_to_show) {
 	return (PROFILE_WEB.content_one === content_to_show)?PROFILE_WEB.content_two:PROFILE_WEB.content_one;
 };
 
-PROFILE_WEB.changePage = function(content_to_show, side) {
-	var content_to_hide = PROFILE_WEB.contentToHide(content_to_show);
-	console.log('content to show: ' + content_to_show);
-	console.log('content to hide: ' + content_to_hide);
+PROFILE_WEB.changePage = function(options) {
+	var element_to_hide = $(PROFILE_WEB.contentToHide(options.selector));
+	var element_to_show = $(options.selector);
+	console.log('show: ' + element_to_show.selector + ' hide: ' + element_to_hide.selector);
 
+	element_to_show.removeClass();
+	element_to_show.addClass('page-swipe-' + options.direction);
 
-	$(content_to_show).toggleClass('page-transition page-visible');
-
-	console.log('empty: ' + content_to_hide);
-	$(content_to_hide).one('webkitTransitionEnd ',
-		function(e) {
-			console.log('empty: ' + content_to_hide);
-			$(content_to_hide).addClass('page-hidden');
-			$(content_to_hide).empty();
-	});
-
-	$(content_to_hide).addClass('page-hidden');
-
+	element_to_show.show();
+	setTimeout(function(){
+        element_to_show.removeClass();
+		element_to_hide.hide();
+		
+    }, 100);
+    
+    element_to_hide.empty();
 };
+
+$('#b_action').bind('click', function() {
+	$('#content-one').prepend('<div class="prepend_div">Test</div>');
+});
+
 
 
 // Place any jQuery/helper plugins in here.
 
 (function($) {
+
 	var app = $.sammy(function() {
 		this.use('Template');
+		var options = null;
 
 		this.get('#/', function() {
 			this.element_selector = PROFILE_WEB.emptyContent();
 			this.render('./templates/home.template').replace(this.element_selector);
 			options = {'selector': this.element_selector, 'direction':'left'};
-			this.trigger('swipe-page', options);
 		});
 
 		this.get('#/gallery', function() {
@@ -56,23 +60,27 @@ PROFILE_WEB.changePage = function(content_to_show, side) {
 			//console.log(PROFILE_WEB.emptyContent());
 			this.render('./templates/gallery.template').replace(this.element_selector);
 			options = {'selector': this.element_selector, 'direction':'bottom'};
-			this.trigger('swipe-page', options);
 		});
 
 		this.get('#/about', function() {
 			this.element_selector = PROFILE_WEB.emptyContent();
-			//console.log(PROFILE_WEB.emptyContent());
-			$(PROFILE_WEB.emptyContent()).text('About');
+			console.log(PROFILE_WEB.emptyContent());
+			$(PROFILE_WEB.emptyContent()).html('<span>About</span>');
 			options = {'selector': this.element_selector, 'direction':'right'};
-			this.trigger('swipe-page', options);
 		});
 
-		this.bind('swipe-page', function(event, options) {
-			console.log(options);
-			PROFILE_WEB.changePage(this.element_selector, options);
-		});
+		//this.bindToAllEvents(function(e) {console.log(e);});
+
+		this.bind('swipe-page', function() {
+			console.log('ahora');
+		 	PROFILE_WEB.changePage(options);
+		 });
+
+
 
 	});
+
+	
 
 	$(function() {
 		app.run('#/')
